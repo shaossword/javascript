b133da88416e1f6b33d8250ab21caf1d91f92401
shaossword <shaos@gmx.fr>
2018-12-11 21:49:48 +0100

    pre

---
+++ demandede nom/javademande nom.js
@@ -0,0 +1,9 @@
+var prenom = prompt("comment tu t'appel ?");
+if (prenom.length >=1 && prenom.length<=10){
+    
+    alert("bienvenue " + prenom)
+}
+  
+else {
+    alert("trop long")
+}
---
+++ javaidentifiant/javaidentifiant.js
@@ -0,0 +1,43 @@
+var id = prompt("saisir votre identifiant ");
+var mdp = "12345"
+var user = "lea@gmail.com" 
+if (id.length > 4){
+    alert("bon")
+}
+else {
+    alert("erreur1")
+}
+if console.log('maChaîne.indexOf("@") is '+ maChaîne.indexOf("@"));{
+    alert("bon")
+    }
+else {
+    alert("erreur2")
+    }
+
+if (id == user){
+    alert("bravo")
+}
+else {
+    alert("erreur3")
+}
+var mdp = prompt("combien sont les nains dans les 7 nains ?");
+
+if (mdp == 12345){
+    alert("felicitation")
+}
+else {
+    alert ("erreur4")
+}
+/**Echauffement JS 3
+
+● Demander l'identifiant puis le mot de passe
+
+● vérifier que :
+
+    l'identifiant saisi a plus de 4 caractères
+
+    l'identifiant contient un @
+
+    l'identifiant est "lea@gmail.com" et son mot de passe "12345"
+
+Bonus : reprendre l'exercice login/password en acceptant cette fois plusieurs paires mail/mdp et en donnant 4 essais à l'utilisateur.*/
